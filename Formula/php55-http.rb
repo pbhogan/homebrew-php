f152714e42c4dfc9e4234d895e411b3cb27c8d52
Patrick Hogan <pbhogan@gmail.com>
2016-10-11 09:32:36 -0500

    …

--- Formula/php55-http.rb
+++ Formula/php55-http.rb
@@ -4,7 +4,8 @@ class Php55Http < AbstractPhp55Extension
   init
   homepage 'http://pecl.php.net/package/pecl_http'
   url 'http://pecl.php.net/get/pecl_http-1.7.6.tgz'
-  sha1 '44e28b6d2d9d352c5b949f14bc4dd612e4365c85'
+  # sha1 '44e28b6d2d9d352c5b949f14bc4dd612e4365c85'
+  sha256 'e87207a7dcd713bef0eb3701b552e0d173fbd73dbd52f9d3968851112e899983'
   head 'http://svn.php.net/repository/pecl/http/trunk/'
 
   def install
@@ -19,3 +20,4 @@ class Php55Http < AbstractPhp55Extension
     write_config_file unless build.include? "without-config-file"
   end
 end
+
